7275b6912e3b8239451caa57801debf57c0a2aca
mertolbekirov <mertol2002@gmail.com>
2021-04-25 13:52:19 +0300

    Define FindDMAAddy function

--- AssaultCubeExternalCheat/proc.cpp
+++ AssaultCubeExternalCheat/proc.cpp
@@ -27,10 +27,36 @@ DWORD GetProcId(const wchar_t* procName)
 
 uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
 {
-
+	uintptr_t modBaseAddr = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, 0);
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
+		MODULEENTRY32 modEntry;
+		modEntry.dwSize = sizeof(modEntry);
+		if (Module32First(hSnap, &modEntry))
+		{
+			do
+			{
+				if (!_wcsicmp(modEntry.szModule, modName))
+				{
+					modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					break;
+				}
+			} while (Module32Next(hSnap,&modEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return modBaseAddr;
 }
 
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> x)
+//used to derefernce multi-level pointers
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
 {
-
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); i++)
+	{
+		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
+		addr += offsets[i];
+	}
+	return addr;
 }
--- AssaultCubeExternalCheat/proc.h
+++ AssaultCubeExternalCheat/proc.h
@@ -8,4 +8,4 @@ DWORD GetProcId(const wchar_t* procName);
 
 uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
 
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> x);
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
