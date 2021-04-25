76fddbfc590db7ef74ac3fc6469a9b9dd9dbe562
mertolbekirov <mertol2002@gmail.com>
2021-04-25 13:07:00 +0300

    Define GetProcId func

--- AssaultCubeExternalCheat/proc.cpp
+++ AssaultCubeExternalCheat/proc.cpp
@@ -1,1 +1,36 @@
 #include "proc.h"
+
+
+DWORD GetProcId(const wchar_t* procName)
+{
+	DWORD procId = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+		if (Process32First(hSnap, &procEntry))
+		{
+			do
+			{
+				if (!_wcsicmp(procEntry.szExeFile, procName))
+				{
+					procId = procEntry.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnap, &procEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return procId;
+}
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+
+}
+
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> x)
+{
+
+}
