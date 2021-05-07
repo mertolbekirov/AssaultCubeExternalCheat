6e6dc386b2730a19848a44985a80894a88082521
mertolbekirov <mertol2002@gmail.com>
2021-05-07 17:01:03 +0300

    finishing all options

--- BytePatchingAssaultCube/BytePatchingAssaultCube.cpp
+++ BytePatchingAssaultCube/BytePatchingAssaultCube.cpp
@@ -55,5 +55,37 @@ int main()
                 mem::PatchEx((BYTE*)(moduleBase + 0x637e9), (BYTE*)"\xFF\x0E", 2, hProcess);
             }
         }
+
+        if (GetAsyncKeyState(VK_NUMPAD3) & 1)
+        {
+            bRecoil = !bRecoil;
+
+            if (bRecoil)
+            {
+                mem::NopEx((BYTE*)(moduleBase + 0x63786), 19, hProcess);
+            }
+            else
+            {
+                mem::PatchEx((BYTE*)(moduleBase + 0x63786), (BYTE*)"\x50\x8d\x4c\x24\x1c\x51\x8b\xce\xff\xd2", 10, hProcess);
+            }
+        }
+
+        if (GetAsyncKeyState(VK_INSERT) & 1)
+        {
+            return 0;
+        }
+
+        //continuous write or freeze
+
+        if (bHealth)
+        {
+            mem::PatchEx((BYTE*)healthAddr, (BYTE*)&newValue, sizeof(newValue), hProcess);
+        }
+
+        Sleep(10);
     }
+
+    std::cout << "Process not found, press Enter to exit\n";
+    getchar();
+    return 0;
 }
